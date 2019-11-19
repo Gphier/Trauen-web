aae23f2b4400eb3c0224e97164658b81a322696e
Guian Liza <lguianphier@gmail.com>
2019-11-19 00:28:48 -0500

    Trauen

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function ($) {
+  $(function () {
+    $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
